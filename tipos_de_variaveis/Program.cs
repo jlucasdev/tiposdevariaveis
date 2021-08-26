0a0acb6292a6e13b52710e41f6e0a96a1476fffd
João Lucas <joaolucaspdsilva1@gmail.com>
2021-08-25 23:03:18 -0300

    9 commit

---
+++ tipos_de_variaveis/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tipos_de_variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            //i = "s";
+
+            var x = 2;
+
+            //x = "333";
+
+
+            Console.WriteLine(i);
+            Console.WriteLine(x);
+            Console.ReadLine();
+
+
+        }
+    }
+}
